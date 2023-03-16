12174e98a7555e801d6a06bd662eacd1412c35c4
HB-Student <chri00m9@mail.hansenberg.dk>
2023-03-16 13:14:16 +0100

    Cam Update

--- unity eksamen/Assets/moveCam.cs
+++ unity eksamen/Assets/moveCam.cs
@@ -5,8 +5,10 @@ public class moveCam : MonoBehaviour
 {
     public float zoom;
     // Start is called before the first frame update
+    float camZ;
     void Start()
     {
+        camZ = transform.position.z;
     }
 
     // Update is called once per frame
@@ -15,28 +17,27 @@ public class moveCam : MonoBehaviour
         zoom = GetComponent<Camera>().orthographicSize;
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
         worldPosition = (worldPosition*5)/zoom;
-
         if(worldPosition.x>9){
-            transform.position=new Vector3(transform.position.x+(worldPosition.x-9)*0.05f, transform.position.y,-10);
+            transform.position=new Vector3(transform.position.x+(worldPosition.x-9)*0.05f, transform.position.y,camZ);
         }
         if(worldPosition.x<-9){
-            transform.position=new Vector3(transform.position.x-(worldPosition.x+9)*-0.05f, transform.position.y,-10);
+            transform.position=new Vector3(transform.position.x-(worldPosition.x+9)*-0.05f, transform.position.y,camZ);
         }
         if(worldPosition.y>3.5f){
-            transform.position=new Vector3(transform.position.x, transform.position.y+(worldPosition.y+3.5f)*0.005f,-10);
+            transform.position=new Vector3(transform.position.x, transform.position.y+(worldPosition.y+3.5f)*0.005f,camZ);
         }
         if(worldPosition.y<-3.5f){
-            transform.position=new Vector3(transform.position.x, transform.position.y-(worldPosition.y-3.5f)*-0.005f,-10);
+            transform.position=new Vector3(transform.position.x, transform.position.y-(worldPosition.y-3.5f)*-0.005f,camZ);
         }
 
         if(Input.GetKeyDown("space")){
-            transform.position=new Vector2(0,0);
+            transform.position=new Vector3(0,0,camZ);
         }
-        if( Input.GetAxis("Mouse ScrollWheel") > 0f){
-            GetComponent<Camera>().orthographicSize+=0.1f;
-        }
-        if( Input.GetAxis("Mouse ScrollWheel") < 0f && zoom>0.4){
+        if( Input.GetAxis("Mouse ScrollWheel") > 0f && zoom>0.4){
             GetComponent<Camera>().orthographicSize-=0.1f;
+        }
+        if( Input.GetAxis("Mouse ScrollWheel") < 0f){
+            GetComponent<Camera>().orthographicSize+=0.1f;
         }   
     }
 }

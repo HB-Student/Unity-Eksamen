57242dc07249928895326a989cebef47884e70be
Sylvester Lave <sylvester.lave@gmail.com>
2023-03-30 15:54:23 +0200

    levels updated

--- unity eksamen/Assets/archerBranch.cs
+++ unity eksamen/Assets/archerBranch.cs
@@ -14,10 +14,10 @@ public class archerBranch : branch
     // Start is called before the first frame update
     void Start()
     {
-        archer4 = new skill(4, "archer3", "add 4x range to archer");
         archer5 = new skill(4, "archer4", "add 5x range to archer");
-        archer3 = new skill(3, "archer2", new List<skill>() { archer4, archer5 },"add 3x range");
-        archer2 = new skill(2, "archer1", new List<skill>() { archer3 }, "Add 2x agility");
+        archer4 = new skill(4, "archer3", new List<skill>() { archer5 },"add 4x range to archer");
+        archer3 = new skill(3, "archer2", new List<skill>() { archer5 },"add 3x range");
+        archer2 = new skill(2, "archer1", new List<skill>() { archer3, archer4 }, "Add 2x agility");
         archer1 = new skill(1, "archer", new List<skill>() { archer2 }, "enable archers"); archer1.buyable = true;
         skills.Add(archer1);
         skills.Add(archer2);

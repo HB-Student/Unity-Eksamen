8d065ce7d087f4d8dab283981696f96d13dcd3e3
HB-Student <chri00m9@mail.hansenberg.dk>
2023-05-02 09:40:06 +0200

    Assigning enemy based on closest

--- unity eksamen/Assets/Scripts/scanners.cs
+++ unity eksamen/Assets/Scripts/scanners.cs
@@ -5,33 +5,44 @@ using UnityEngine;
 public abstract class scanners : MonoBehaviour
 {
 	public stat sightRadius = new stat("sightRadius");
-    public GameObject enemy;
+	public GameObject enemy;
 
-    public bool scanBool(string opponentTag, float colliderRadius)
-    {
-        Collider2D[] overlapCollider = Physics2D.OverlapCircleAll(transform.position, colliderRadius);
-        for (int i = 0; i < overlapCollider.Length; i++)
-        {
-            if (overlapCollider[i].gameObject.tag == opponentTag)
-            {
-                enemy = overlapCollider[i].gameObject;
-                return true;
-            }
-        }
-        return false;
-    }
+	public bool scanBool(string opponentTag, float colliderRadius)
+	{
+		List<GameObject> enemies = scanList(opponentTag, colliderRadius);
+		if (enemies.Count > 0)
+		{
+			GameObject closestEnemy = enemies[0];
+			float closestDist = Vector2.Distance(transform.position, closestEnemy.transform.position);
+			foreach (var opponent in enemies)
+			{
+				float opponentDist = Vector2.Distance(transform.position, opponent.transform.position);
+				if (opponentDist < closestDist)
+				{
+					closestEnemy = opponent;
+					closestDist = opponentDist;
+				}
+			}
+			enemy = closestEnemy;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
-    public List<GameObject> scanList(string scanningTag, float colliderRadius)
-    {
-        Collider2D[] overlapCollider = Physics2D.OverlapCircleAll(transform.position, colliderRadius);
-        List<GameObject> enemies = new List<GameObject>();
-        for (int i = 0; i < overlapCollider.Length; i++)
-        {
-            if (overlapCollider[i].gameObject.tag == scanningTag)
-            {
-                enemies.Add(overlapCollider[i].gameObject);
-            }
-        }
-        return enemies;
-    }
+	public List<GameObject> scanList(string scanningTag, float colliderRadius)
+	{
+		Collider2D[] overlapCollider = Physics2D.OverlapCircleAll(transform.position, colliderRadius);
+		List<GameObject> enemies = new List<GameObject>();
+		for (int i = 0; i < overlapCollider.Length; i++)
+		{
+			if (overlapCollider[i].gameObject.tag == scanningTag)
+			{
+				enemies.Add(overlapCollider[i].gameObject);
+			}
+		}
+		return enemies;
+	}
 }
